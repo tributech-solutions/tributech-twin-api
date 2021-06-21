e3b2cecd0c3768437504773de5220681b3d95f22
Maximilian Mayr <m.mayr@tributech.io>
2021-06-21 14:54:54 +0200

    feature(scaffold): initial push

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# tributech-twin-api
-Twin Instance service used to manage instances of digital twins
+# Tributech Twin API
+
+## Description
+The Tributech Twin API service gets used to manage instances of digital twins written in the [Digital Twin Definition Language](https://github.com/Azure/opendigitaltwins-dtdl/blob/master/DTDL/v2/dtdlv2.md).
+
+Responsibilities:
+- Validate instances
+- Get instances
+- Add/Edit instances
+- Remove instances 
+- Get relationships
+- Add/Edit relationships
+- Remove relationships
+
+
+## Development
+
+### dotnet
+To access our private NuGet feed at Azure DevOps install [Azure Artifacts Credential Provider
+](https://github.com/microsoft/artifacts-credprovider#setup) plugin and restore NuGet packages with:
+```powershell
+dotnet restore --interactive` # (interactive is only required at first time)
+dotnet build
+```
+More info: [Guidelines - Azure DevOps - NuGet packages](https://tributech.atlassian.net/wiki/spaces/TM/pages/1005781050/NuGet+packages#dotnet-build-%28local%29)
+
+### Docker
+
+#### Build images
+```powershell
+docker-compose -f ./docker-compose.yml -f ./docker-compose.ci.build.yml build
+```
+The PAT (personal access token) must have read permissions for private NuGet feed at Azure DevOps for central package repository (see `nuget.config`).
+More info: [Guidelines - Azure DevOps - NuGet packages](https://tributech.atlassian.net/wiki/spaces/TM/pages/1005781050/NuGet+packages#Docker-based-build-%28local%29)
---
+++ src/Tributech.DataSpace.TwinAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tributech.DataSpace.TwinAPI.Options;
+
+namespace Tributech.DataSpace.TwinAPI.Extensions {
+	public static class ServiceCollectionExtensions {
+		/// <summary>
+		/// Helper to configure options with some handy defaults and out param for instance (e.g. for <see cref="ApiAuthOptions"/>).
+		/// </summary>
+		/// <typeparam name="TOptions">The options type.</typeparam>
+		/// <param name="services">The service collection to register options at.</param>
+		/// <param name="configuration">The configuration.</param>
+		/// <param name="options">Instance of the options.</param>
+		/// <param name="configSectionName">(Optional) The config section name. Default: Name of TOptions type.</param>
+		/// <param name="optionsInstanceName">(Optional) The name of the options instance. Default: "" (as defined in <see cref="Microsoft.Extensions.Options.Options.DefaultName"/>)</param>
+		/// <returns>The service collection.</returns>
+		public static IServiceCollection AddOptions<TOptions>(this IServiceCollection services, IConfiguration configuration, out TOptions options, string? configSectionName = null, string optionsInstanceName = "")
+			where TOptions : class, new() {
+			configSectionName ??= typeof(TOptions).Name;
+			IConfigurationSection apiAuthOptionsSection = configuration.GetSection(configSectionName);
+			options = apiAuthOptionsSection.Get<TOptions>() ?? new TOptions();
+			services.AddOptions<TOptions>(optionsInstanceName).Bind(apiAuthOptionsSection);
+			return services;
+		}
+
+		/// <summary>
+		/// Helper to configure options with some handy defaults (e.g. for <see cref="ApiAuthOptions"/>).
+		/// </summary>
+		/// <typeparam name="TOptions">The options type.</typeparam>
+		/// <param name="services">The service collection to register options at.</param>
+		/// <param name="configuration">The configuration.</param>
+		/// <param name="configSectionName">(Optional) The config section name. Default: Name of TOptions type.</param>
+		/// <param name="optionsInstanceName">(Optional) The name of the options instance. Default: "" (as defined in <see cref="Microsoft.Extensions.Options.Options.DefaultName"/>)</param>
+		/// <returns>The service collection.</returns>
+		public static IServiceCollection AddOptions<TOptions>(this IServiceCollection services, IConfiguration configuration, string? configSectionName = null, string optionsInstanceName = "")
+			where TOptions : class, new()
+			=> AddOptions<TOptions>(services, configuration, out _, configSectionName, optionsInstanceName);
+	}
+}
