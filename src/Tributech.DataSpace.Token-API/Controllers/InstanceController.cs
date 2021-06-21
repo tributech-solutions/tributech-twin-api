14118d60980629718dc7cb48bc8f5ad1cc15a121
Maximilian Mayr <m.mayr@tributech.io>
2021-06-21 14:54:54 +0200

    feature(scaffold): initial push

---
+++ src/Tributech.DataSpace.Token-API/Controllers/InstanceController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Tributech.DataSpace.TwinAPI.Model;
+
+namespace Tributech.DataSpace.TwinAPI.Controllers {
+
+	//[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	[Route("[controller]")]
+	[ApiController]
+	public class InstanceController : ControllerBase {
+		private readonly ILogger<InstanceController> _logger;
+
+		public InstanceController(ILogger<InstanceController> logger) {
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<BaseDigitalTwin> Get() {
+			return new BaseDigitalTwin[] {
+			new BaseDigitalTwin() {
+					Id = "tets",
+					ETag = "tetet",
+					Properties = new Dictionary<string, JsonElement> (),
+					Metadata = new DigitalTwinMetadata() {
+						ModelId = "dtmi:io:tributech:test;1"
+					}
+				}
+			};
+		}
+
+		// GET api/<InstanceController>/5
+		[HttpGet("{dtid}")]
+		public string Get(int dtid) {
+			return "value";
+		}
+
+		// POST api/<InstanceController>
+		[HttpPost]
+		public void Post([FromBody] BaseDigitalTwin twin) {
+			string dictionaryString = "{";
+			foreach (KeyValuePair<string, JsonElement> keyValues in twin?.Properties) {
+				dictionaryString += keyValues.Key + " : " + keyValues.Value.ToString() + ", ";
+			}
+			_logger.LogWarning(dictionaryString.TrimEnd(',', ' ') + "}");
+		}
+
+		// PUT api/<InstanceController>/5
+		[HttpPut("{dtid}")]
+		public void Put(int dtid, [FromBody] BaseDigitalTwin twin) {
+		}
+
+		// DELETE api/<InstanceController>/5
+		[HttpDelete("{dtid}")]
+		public void Delete(int dtid) {
+		}
+	}
+}
---
+++ src/Tributech.DataSpace.Token-API/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Tributech.DataSpace.Common.Auth;
+using Tributech.DataSpace.Common.Auth.ApiAuth;
+using Tributech.DataSpace.TwinAPI.Utils;
+
+namespace Tributech.DataSpace.TwinAPI {
+	public class Startup {
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddOptions(Configuration, out ApiAuthOptions apiAuthOptions);
+
+			//var configExample = Configuration.GetSection(nameof(ClassName));
+			//services.AddOptions<ClassName>().Bind(configExample));
+
+			//services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+			//	.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => {
+			//		options.Authority = apiAuthOptions.Authority;
+			//		options.Audience = apiAuthOptions.Audience;
+			//	});
+
+			services.AddHealthChecks();
+			services.AddRouting(options => options.LowercaseUrls = true);
+
+			services.AddControllers();
+			services.AddSwaggerCustom(apiAuthOptions);
+
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<ApiAuthOptions> apiAuthOptionsAccessor) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+			}
+		
+			app.UseRouting();
+
+			//app.UseAuthentication();
+			//app.UseAuthorization();
+
+			app.UseEndpoints(endpoints => {
+				endpoints.MapControllers();
+			});
+
+			app.UseSwaggerCustom(apiAuthOptionsAccessor.Value);
+		}
+	}
+}
